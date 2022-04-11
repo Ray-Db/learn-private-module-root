8a439f4ce16ad4b60babf17fbf244c0dbbbd7cba
Ray-Db <33378206+Ray-Db@users.noreply.github.com>
2022-04-11 17:56:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 module "module-aws-s3" {
-  source  = "app.terraform.io/raycomp/terraform/module-aws-s3/aws"
+  source      = "app.terraform.io/raycomp/module-aws-s3/private"
   name        = var.name
-  region = var.region
-  prefix = var.prefix
-  version = "1.0.0"
+  region      = var.region
+  prefix      = var.prefix
+  version     = "1.0.0"
 }
